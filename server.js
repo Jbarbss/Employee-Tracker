8dd9b61cc19b1717a9f63930632e4cb8920937b9
Jbarbss <jbarbanel@gmail.com>
2021-04-30 11:01:55 -0400

    added function for viewallemployees

--- server.js
+++ server.js
@@ -1,77 +1,91 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-const consTable = require('console.table');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consTable = require("console.table");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
 
-  // Your port; if not 3306
+  // port 3306
   port: 3306,
 
-  // Your username
-  user: 'root',
+  //username
+  user: "root",
 
   // Be sure to update with your own MySQL password!
-  password: 'password',
-  database: 'employee_tracker_db',
+  password: "password",
+  database: "employee_tracker_db",
 });
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log("Server is Live")
+  console.log("Server is Live");
   console.log("\n WELCOME TO EMPLOYEE TRACKER \n");
-  runSearch();
+  startProgram();
 });
 
+// starting program with prompt
+
 function startProgram() {
-  inquirer.prompt([
-    {
-      type: 'choice',
-      type: 'list',
-      message: 'Please choose from one of the following options',
-      choices: [
-        "View All Employees",
-        "View All Employees by Roles",
-        "View All Employees by Department",
-        "Update Employee",
-        "Add Employee",
-        "Add Role",
-        "Add Department"
-        
-      ]
-    }
-  ])
-  .then(function(val) {
-    switch (val.choice) {
-      case  "View All Employees":
-        viewEmployees();
-      break;
+  inquirer
+    .prompt([
+      {
+        name: "choice",
+        type: "list",
+        message: "Please choose from one of the following options",
+        choices: [
+          "View All Employees",
+          "View All Employees by Roles",
+          "View All Employees by Department",
+          "Update Employee",
+          "Add Employee",
+          "Add Role",
+          "Add Department",
+        ],
+      },
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case "View All Employees":
+          viewEmployees();
+          break;
 
-      case  "View All Employees by Roles":
-        viewRoles();
-      break;
+        case "View All Employees by Roles":
+          viewRoles();
+          break;
 
-      case  "View All Employees by Department":
-        viewDepartments();
-      break;
+        case "View All Employees by Department":
+          viewDepartments();
+          break;
 
-      case  "Update Employee":
-        updateEmployee();
-      break;
+        case "Update Employee":
+          updateEmployee();
+          break;
 
-      case  "Add Employee":
-        addEmployee();
-      break;
+        case "Add Employee":
+          addEmployee();
+          break;
 
-      case  "Add Role":
-        addRole();
-      break;
-      
-      case  "Add Department":
-        addDepartment();
-      break;
+        case "Add Role":
+          addRole();
+          break;
 
-
-    }  
-  })
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+    });
 }
+
+// function to view all employees
+
+const viewEmployees = () => {
+  console.log("-----------------");
+  connection.query(
+    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      startProgram();
+    }
+  );
+};
