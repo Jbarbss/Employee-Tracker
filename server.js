2cdf4b097e537137fbc45f5b594f14fdf1e3fe70
Jbarbss <jbarbanel@gmail.com>
2021-04-30 11:11:27 -0400

    added functions for vew department

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ function startProgram() {
 const viewEmployees = () => {
   console.log("-----------------");
   connection.query(
-    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
     (err, res) => {
       if (err) throw err;
       console.table(res);
@@ -89,3 +89,17 @@ const viewEmployees = () => {
     }
   );
 };
+
+const viewDepartments = () => {
+  console.log("-----------------");
+    connection.query(
+      "SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;",     
+      (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      startProgram();
+      }
+    )
+  };
+
+
